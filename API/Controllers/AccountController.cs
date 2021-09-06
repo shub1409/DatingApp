0986fb0d5b7938e01a3c8f6b02ba345988f9cadf
Shubham Gupta <shub1409@gmail.com>
2021-09-06 06:57:25 +0530

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -48,6 +49,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
+                .Include(p => p.Photos)
                 .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
 
             if (user == null)
@@ -63,7 +65,11 @@ namespace API.Controllers
                     return Unauthorized("Invalid Password");
             }
 
-            return new UserDto { UserName = user.UserName, Token = _tokenService.CreateToken(user) };
+            return new UserDto { 
+                UserName = user.UserName, 
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+            };
 
         }
         private async Task<bool> UserExists(string username)
